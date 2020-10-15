da0db9df4c4d161bd296084fbc276530e11fc922
jingyi-stats <tangjingyi1995@163.com>
2020-10-15 14:46:14 -0400

    trial on python syntax

--- first_python.py
+++ first_python.py
@@ -1,31 +1,21 @@
 # , will be shown as a space in the output
 print('The quick brown fox', 'jumps over', 'the lazy dog')
-
 print(300)
-
 print(100 + 200)
-
 # here the python treated 100 + 200 = as string
 print("100 + 200 =", 100 + 200)
 
 # wait for an input to be "name"
 name = input()
-
 # show the name that was input in last step
 name
-
 # print name
 print(name)
-
 name = input()
-
 # print hello, [the name you input]
 print('hello,', name)
-
 # to have a prompt in the print()
 name = input('please enter your name: ')
-
 print('hello,', name)
-
 # PRACTICE
 print("1024 * 768 =", 1024 * 768)
