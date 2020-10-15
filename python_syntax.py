5addd4e13f152aeeee1ea7df5a1375a254cc0688
jingyi-stats <tangjingyi1995@163.com>
2020-10-15 16:24:12 -0400

    added on python_sybtax with 2 more if statements

--- python_syntax.py
+++ python_syntax.py
@@ -6,3 +6,28 @@ if a >= 0:
     print(a)
 else:
     print(-a)
+
+# print if the input age is adult or not
+year = input("Please enter your birth year: ")
+
+print(2020 - int(year))
+
+a = int(2020 - int(year))
+if a >= 18:
+    print("Adult")
+else:
+    print("underage")
+
+# print what to do next
+condition = input("What can I help you with?")
+
+if condition == "hungry":
+    print("Please eat.")
+elif condition == "tired":
+    print("Get some rest.")
+elif condition == "sick":
+    print("Go to a doctor.")
+elif condition == "chill":
+    print("Go to study.")
+else:
+    print("Sorry, I don't understand your condition.")
